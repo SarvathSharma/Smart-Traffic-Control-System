b0a319970642593c49ed5e19eeb631228521c47c
SarvathSharma <sarvath2000@gmail.com>
2020-06-09 17:09:57 -0500

    Added Vehicle Count :oncoming_automobile:

--- Video Rendering/Traffic.m
+++ Video Rendering/Traffic.m
@@ -2,25 +2,32 @@ clc;	% Clear command window.
 clear;	% Delete all variables.
 
 trafficVid = VideoReader('TrafficTest2.mp4'); % Reading in video
-nframes = trafficVid.NumFrames; % Calculating how many frames are in the video
+nframes = trafficVid.NumFrames; % Calculating number of frames
 
-%Train model using the first 150 frames
+% Train model using the first 150 frames
 foregroundDetector = vision.ForegroundDetector('NumGaussians', 3, ...
     'NumTrainingFrames', 150);
 
 for k = 1 : nframes
+    %Read frame
+    singleFrame = readFrame(trafficVid);
     
-    singleFrame = readFrame(trafficVid); %Read frame
+    % Inital image filtering
+    foreground = step(foregroundDetector, singleFrame);
     
-    foreground = step(foregroundDetector, singleFrame); %Inital image filtering
+    % Convert to grayscale to do morphological processing
+    newImgs = imageEnhancement(foreground);
     
-    % Convert to grayscale to do morphological processing.
-    I = imageEnhancement(foreground);
-    
-    I = vehicleDetection(I, singleFrame); % Blob analysis
-    
-    imshow(I); %The first 150 outputs are used for training purposes
-
+    % We will display data after the model has trained
+    if k > foregroundDetector.NumTrainingFrames
+        % Blob analysis
+        title('Detecting Vehicles')
+        detectedVehicles = vehicleDetection(newImgs, singleFrame);
+        imshow(detectedVehicles);
+    else
+        text(10,10,'\color{green}Calibrating...')
+        imshow(singleFrame)
+    end
 end
 
 function img = imageEnhancement(input)
@@ -48,14 +55,15 @@ function img = imageEnhancement(input)
     img = finalImg;
 end
 
-function img = vehicleDetection(input, frame)   
-    
-    %Performs blob analysis in order to create a green box around cars
+function result = vehicleDetection(input, frame)   
+    % Performs blob analysis in order to create a green box around cars
+    % Then count the number of boxes which should be the cars
     blobAnalysis = vision.BlobAnalysis('BoundingBoxOutputPort', true, ...
         'AreaOutputPort', false, 'CentroidOutputPort', false, ...
         'MinimumBlobArea', 250, 'ExcludeBorderBlobs', true);
     bbox = step(blobAnalysis, input);
-    img = insertShape(frame, 'Rectangle', bbox, 'Color', 'green');
-%     text(10,10,strcat('\color{black}Vehicles Detected:', ...
-%         )
+    result = insertShape(frame, 'Rectangle', bbox, 'Color', 'green');
+    numberOfVehicles = size(bbox, 1);
+    result = insertText(numberOfVehicles, [15 15], numberOfVehicles,...
+                'BoxOpacity', 1, 'FontSize', 15);
 end
