52ee53803aac8d9a8d4da3d6ba1d6de557a53ffa
SarvathSharma <sarvath2000@gmail.com>
2020-08-03 14:20:55 -0500

    Added method to start MATLAB process

--- pipelineIntegration/main.py
+++ pipelineIntegration/main.py
@@ -1,8 +1,18 @@
 # This is a python script that allows us to integrate with the MATLAB system
 # The main purpose is to grab and analyze the data from the MATLAB script
 
+import sys
 import matlab.engine
-
 eng = matlab.engine.start_matlab()
 
-print 'Hello World'
+positionOfPath = 1
+sys.path.insert(positionOfPath, '../vehicleDetection/motionTracking.m')
+
+
+# There are two ways we can do this. Either run it as a script or run it as a function
+
+# Run it as a function
+
+# Pass in inputs to the function
+# ret = eng.MotionBasedMultiObjectTracking( *Need parameters here* )
+# print(ret)
