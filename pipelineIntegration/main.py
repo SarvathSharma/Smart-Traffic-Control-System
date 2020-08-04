fde0a6507a73fe9253e4596a0579186d0c9bbe94
SarvathSharma <sarvath2000@gmail.com>
2020-08-04 16:49:02 -0500

    Python script will now start the MATLAB engine and get the CSV file generated

--- pipelineIntegration/main.py
+++ pipelineIntegration/main.py
@@ -1,18 +1,41 @@
 # This is a python script that allows us to integrate with the MATLAB system
 # The main purpose is to grab and analyze the data from the MATLAB script
 
+# All the libraries needed
+import os.path
+import re
+import shutil
 import sys
 import matlab.engine
 
-positionOfPath = 1
-sys.path.insert(positionOfPath, '../vehicleDetection/motionTracking.m')
+# Get info on current directory and check files
+# Then move to the directory where the MATLAB script is
+currDir = os.getcwd()
+print("Current directory " + currDir)
+os.chdir("../vehicleDetection")
+projectDir = os.getcwd()
+print("Moved to " + projectDir)
+arr = os.listdir(projectDir)
+print("Initial files in directory " + str(arr))
 
+# Start the MATLAB engine and run the motionTracking script
+# After running the script show the new files in the directory
 eng = matlab.engine.start_matlab()
+eng.motionTracking(nargout=0)
+arr2 = os.listdir(projectDir)
+print("Final files in directory " + str(arr2))
 
-# There are two ways we can do this. Either run it as a script or run it as a function
+# Given the list of the new files check each one of them and find the csv file
+for x in arr2:
+    fileToMove = re.match(r".*\.csv", x)
+    if fileToMove:
+        # If the csv file is found, move it to the same directory as the Python script
+        print("File found: " + x)
+        newPath = shutil.move(x, currDir)
+        print(x + " moved to " + currDir)
+        sys.exit()
+    else:
+        # If it was not found let the user know, then if none match print an error
+        print('File not matching regex key, moving to next one')
 
-# Run it as a function
-
-# Pass in inputs to the function
-# ret = eng.MotionBasedMultiObjectTracking( *Need parameters here* )
-# print(ret)
+print('No CSV file was found, check MATLAB script for errors')
