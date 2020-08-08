5f39463a25aeae7e05dece3b915a3ff436c2bc24
Sathira <sathirakatugaha@gmail.com>
2020-08-07 20:50:33 -0400

    initial version of upload

--- SmartTrafficApp.py
+++ SmartTrafficApp.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploaded-videos'
+ALLOWED_EXTENSIONS = {'mp4', 'MP4', '.avi', '.AVI'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-@app.route('/home')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return render_template('index.html')
-
-
-@app.route('/upload')
-def upload():
-    return render_template('upload.html')
 
 @app.route('/aboutus')
 def aboutus():
