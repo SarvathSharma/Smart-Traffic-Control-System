c2c79273fca38e4d6332abcebc3f0b5abc48fc78
patbeqo <patbeqo@gmail.com>
2020-08-07 19:49:07 -0700

    Added loading widget

---
+++ static/jsfiles/index.js
@@ -0,0 +1,5 @@
+var loadingAnimation = document.getElementById('loading-animation')
+
+document.getElementById('myform').addEventListener('submit', (e) => {
+  loadingAnimation.style.display = 'block';
+});
