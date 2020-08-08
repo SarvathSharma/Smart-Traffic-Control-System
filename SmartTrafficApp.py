9e26321d65c3c6b578faff0d2fa89147da222c99
SarvathSharma <sarvath2000@gmail.com>
2020-08-07 21:24:34 -0500

    Updated integration to run in one Python script

--- SmartTrafficApp.py
+++ SmartTrafficApp.py
@@ -1,5 +1,10 @@
 import csv
 import os
+import os.path
+import re
+import shutil
+import sys
+import matlab.engine
 from flask import Flask, render_template, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
@@ -10,9 +15,45 @@ app = Flask(__name__)
 app.secret_key = 'patrikbicho'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
     extension = '.' in filename and filename.rsplit('.', 1)[1].lower()
     return '.' in filename and extension in ALLOWED_EXTENSIONS and extension
+
+
+def run_matlab():
+    # Get info on current directory and check files
+    # Then move to the directory where the MATLAB script is
+    currDir = os.getcwd()
+    print("Current directory " + currDir)
+    os.chdir("./vehicleDetection")
+    projectDir = os.getcwd()
+    print("Moved to " + projectDir)
+    arr = os.listdir(projectDir)
+    print("Initial files in directory " + str(arr))
+
+    # Start the MATLAB engine and run the motionTracking script
+    # After running the script show the new files in the directory
+    eng = matlab.engine.start_matlab()
+    eng.motionTracking(nargout=0)
+    arr2 = os.listdir(projectDir)
+    print("Final files in directory " + str(arr2))
+
+    # Given the list of the new files check each one of them and find the csv file
+    for x in arr2:
+        fileToMove = re.match(r".*\.csv", x)
+        if fileToMove:
+            # If the csv file is found, move it to the same directory as the Python script
+            print("File found: " + x)
+            newPath = shutil.move(x, currDir)
+            print(x + " moved to " + currDir)
+            sys.exit()
+        else:
+            # If it was not found let the user know, then if none match print an error
+            print('File not matching regex key, moving to next one')
+
+    print('No CSV file was found, check MATLAB script for errors')
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
@@ -28,6 +69,11 @@ def home():
         print("allowed extension " + allowedExtension if allowedExtension is not False else "")
         if file and allowedExtension is not False:
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], "traffic-test." + allowedExtension))
+        else:
+            # Remove the given CSV file and then run the MATLAB script to get the new data file
+            os.remove('finalData.csv')
+            run_matlab()
+            return render_template('index.html', data=graphData)
 
     # Opening csv file
     if request.method == 'GET':
@@ -45,9 +91,11 @@ def home():
           print(graphData)
     return render_template('index.html', data=graphData)
 
+
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
