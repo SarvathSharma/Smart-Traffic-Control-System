7bfcb99c76de9759e3522128f12dd8bd16e921a1
SarvathSharma <sarvath2000@gmail.com>
2020-08-16 18:10:52 -0500

    Fixed typo

--- SmartTrafficApp.py
+++ SmartTrafficApp.py
@@ -129,4 +129,4 @@ def aboutus():
 if __name__ == '__main__':
     app.run(debug=True)
     # On linux server
-    # app.run(debug=True, host='0.0.0.0', port=5000, threaded=true)
+    # app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
