60c110b6018009db11a6e053b217a8bd3147ca54
patbeqo <patbeqo@gmail.com>
2020-09-04 16:02:57 -0700

    firesale :fire:

--- SmartTrafficApp.py
+++ SmartTrafficApp.py
@@ -1,96 +1,33 @@
 import csv
-import os
-from MatlabProcessor import run_matlab
-import shutil
-from os import path
-from os.path import join
 from flask import Flask, render_template, flash, request, redirect, url_for
-from werkzeug.utils import secure_filename
-import redis
-from rq import Queue
-
-ALLOWED_EXTENSIONS = {'mp4', 'MP4'}
 
 app = Flask(__name__)
 app.secret_key = 'oursecretkey'
 
-APP_ROOT = path.dirname(path.abspath(__file__))
-UPLOAD_FOLDER = join(APP_ROOT, 'static', 'uploads')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-graphData = None
-error = False
-extensionError = False
-
-def allowed_file(filename):
-    extension = '.' in filename and filename.rsplit('.', 1)[1].lower()
-    return '.' in filename and extension in ALLOWED_EXTENSIONS and extension
-
-
-def get_data(response):
-    global graphData
-    global error
-    global extensionError
-    # In Linux server
-    # if response and path.exists('/root/Smart-Traffic-Control-System/vehicleDetection/finalData.csv'):
-    if response and path.exists('./vehicleDetection/finalData.csv'):
-        # In linux server
-        # with open('/root/Smart-Traffic-Control-System/vehicleDetection/finalData.csv', mode='r') as csv_file:
-        with open('./vehicleDetection/finalData.csv', mode='r') as csv_file:
-            # Grab Data
-            data = list(csv.reader(csv_file))[0]
-            numPlots = len(data)
-            timeIntervals = []
-            numCars = []
-            for i in range(1, numPlots+1):
-                timeIntervals.append(i * 10)
-            for element in data:
-                numCars.append(int(element))
-            graphData = [timeIntervals, numCars]
-            error = False
-            extensionError = False
-    else:
-        graphData = None
-        error = True
-        extensionError = False
+def get_data(fileName):
+    # In linux server
+    # with open('/root/Smart-Traffic-Control-System/vehicleDetection/{fileName}'.format(fileName = fileName), mode='r') as csv_file:
+    with open('./vehicleDetection/{fileName}'.format(fileName = fileName), mode='r') as csv_file:
+        # Grab Data
+        data = list(csv.reader(csv_file))[0]
+        numPlots = len(data)
+        timeIntervals = []
+        numCars = []
+        for i in range(1, numPlots+1):
+            timeIntervals.append(i * 10)
+        for element in data:
+            numCars.append(int(element))
+        return [timeIntervals, numCars]
 
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home():
-    global extensionError
-    global error
-    global graphData
-    if request.method == 'POST':
-        print(request)
-        print(request.files)
-        if 'file' not in request.files:
-            print('no file part')
-            flash('No file part')
-            return redirect(url_for('home'))
-        file = request.files['file']
-        if file.filename == '':
-            print('no selected file')
-            flash('No selected file')
-            return redirect(url_for('home'))
-        allowedExtension = allowed_file(file.filename)
-        print("allowed extension " + allowedExtension if allowedExtension is not False else "")
-        if file and allowedExtension is not False:
-            print('creating file')
-            savePath = join(UPLOAD_FOLDER, "traffic-test." + allowedExtension)
-            file.save(savePath)
-            job = q.enqueue(run_matlab, "")
-            res = job.result
-            get_data(res)
-        else:
-            extensionError = True
-            error = False
-            graphData = None
-        return redirect(url_for('home'))
-
-    # Opening csv file
-    if request.method == 'GET':
-        return render_template('index.html', data=graphData, error=error, extensionError=extensionError)
+    # Get Data
+    graphData[0] = get_data('finalData.csv')
+    graphData[1] = get_data('finalData2.csv')
+    graphData[2] = get_data('finalData3.csv')
+    return render_template('index.html', data=graphData)
 
 
 @app.route('/aboutus')
--- static/jsfiles/index.js
+++ static/jsfiles/index.js
@@ -1,23 +1,15 @@
 var loadingAnimation = document.getElementById('loading-animation');
 var graph = document.getElementById('graph');
-var helpText = document.getElementById('help-text');
-var errorText = document.getElementById('error-text');
-var extensionErrorText = document.getElementById('extension-error-text');
 var routingLinks = document.getElementById('routing-links');
 
-document.getElementById('upload-form').addEventListener('submit', (e) => {
+document.getElementById('select-button').addEventListener('click', (e) => {
   routingLinks.style.pointerEvents = 'none';
   loadingAnimation.style.display = 'block';
-  if(graph){
-    graph.style.display = 'none';
-  }
-  if(helpText){
-    helpText.style.display = 'none';
-  }
-  if(errorText){
-    errorText.style.display = 'none';
-  }
-  if(extensionErrorText){
-    extensionErrorText.style.display = 'none';
-  }
+  graph.style.display = 'none';
+  setTimeout(() => { 
+    alert("Hello"); 
+  }, 2000);
+  loadingAnimation.style.display = 'none';
+  routingLinks.style.pointerEvents = 'block';
+  graph.style.display = 'block';
 });
